f19468064da259a1422de4b3d34428ae9e551fdf
Ozgur Ersoz <zersz@paypal.com>
2024-02-12 14:52:53 +0100

    update2

--- GCDWebUploader/GCDWebUploader.h
+++ GCDWebUploader/GCDWebUploader.h
@@ -29,6 +29,33 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+#if SWIFT_PACKAGE && !(defined(SWIFTPM_MODULE_BUNDLE))
+
+NSBundle* SWIFTPM_MODULE_BUNDLE() {
+    NSString *bundleName = @"GCDWebServer_GCDWebServer";
+
+
+    NSArray<NSURL*> *candidates = @[
+        NSBundle.mainBundle.resourceURL,
+        NSBundle.mainBundle.bundleURL
+    ];
+
+    for (NSURL* candiate in candidates) {
+        NSURL *bundlePath = [candiate URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.bundle", bundleName]];
+
+        NSBundle *bundle = [NSBundle bundleWithURL:bundlePath];
+        if (bundle != nil) {
+            return bundle;
+        }
+    }
+
+    @throw [[NSException alloc] initWithName:@"SwiftPMResourcesAccessor" reason:[NSString stringWithFormat:@"unable to find bundle named %@", bundleName] userInfo:nil];
+}
+
+#define SWIFTPM_MODULE_BUNDLE SWIFTPM_MODULE_BUNDLE()
+
+#endif
+
 @class GCDWebUploader;
 
 /**
